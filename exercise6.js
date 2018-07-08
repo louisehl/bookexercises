46e73573c1207f240d907151914f1ce1da74427e
Louise <Louiselastein@hotmail.com>
2018-07-08 23:06:27 +0200

    Added exercise 6.2

--- exercise6.js
+++ exercise6.js
@@ -25,3 +25,45 @@ console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
 console.log(new Vec(3, 4).length);
 // → 5
+
+
+
+//6.2
+
+class Group {
+  constructor(){
+    this.array = [];
+  }
+  
+  add(value){
+    if(!this.has(value)){
+      this.array.push(value);
+    }
+  }
+  
+  has(value){
+    return this.array.includes(value);
+  }
+  
+  delete(value){
+    this.array = this.array.filter(x => x != value);
+  }
+  
+  static from(value){
+    let newgroup = new Group();
+    for (let x of value){
+      newgroup.add(x);
+    }
+    console.log(newgroup);
+    return newgroup;
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
