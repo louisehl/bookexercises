1ae9867f8a4d55ef774467177fe7b4a64ac23475
Louise <Louiselastein@hotmail.com>
2018-07-25 15:15:10 +0200

    Færdig med 9.3

--- elojs/exercise9.js
+++ elojs/exercise9.js
@@ -59,3 +59,24 @@ let text = "'I'm the cook,' he said, 'it's my job.'";
 //virker dog ikke hvis der ikke er komma, eller lignende lige inden...
 console.log(text.replace(/(^|\W)'|($|\W)'/g,'$1"$2'));
 // → "I'm the cook," he said, "it's my job."
+
+
+
+// 9.3
+
+// Fill in this regular expression.
+let number = /^[+-]?(\.\d+|\d+(\.$|\.\d+)?([eE][+-]?\d+)?)$/;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
