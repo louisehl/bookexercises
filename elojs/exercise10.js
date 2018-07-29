8c4e1bf6ceaf03bec229ba3370e163d1d8f41d47
Louise <Louiselastein@hotmail.com>
2018-07-29 17:23:28 +0200

    Tilføjet 10.3

--- elojs/exercise10.js
+++ elojs/exercise10.js
@@ -21,7 +21,7 @@
  * I would prefer to write them myself, however save time by using pre-
  * made modules from NPM.
  */
- 
+
 
 // 10.2
 
@@ -38,3 +38,18 @@ const roads = [
 ];
 
 exports.roadGraph = buildGraph(roads.map(r => r.split('-')));
+
+
+
+// 10.3 ; solution from book.
+
+ /* The trick is that require adds modules to its cache before it starts 
+  * loading the module. That way, if any require call made while it is running 
+  * tries to load it, it is already known, and the current interface will be 
+  * returned, rather than starting to load the module once more (which would 
+  * eventually overflow the stack).
+  * If a module overwrites its module.exports value, any other module that has
+  * received its interface value before it finished loading will have gotten 
+  * hold of the default interface object (which is likely empty), rather than 
+  * the intended interface value.
+  */
