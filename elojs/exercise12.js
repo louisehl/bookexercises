fedcf5525281c41574f260944127a0f9a6bb2954
Louise <Louiselastein@hotmail.com>
2018-09-11 22:48:25 +1000

    12.1

---
+++ elojs/exercise12.js
@@ -0,0 +1,19 @@
+//12.1
+
+topScope.array = (...x) => x;
+
+topScope.length = x => x.length;
+
+topScope.element = (arr, n) => arr[n];
+
+run(`
+do(define(sum, fun(array,
+     do(define(i, 0),
+        define(sum, 0),
+        while(<(i, length(array)),
+          do(define(sum, +(sum, element(array, i))),
+             define(i, +(i, 1)))),
+        sum))),
+   print(sum(array(1, 2, 3))))
+`);
+// → 6
