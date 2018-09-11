e8ccd4ef634ac10e945b4f5168116fa30cf24e87
Louise <Louiselastein@hotmail.com>
2018-09-11 23:18:47 +1000

    12.3

--- elojs/exercise12.js
+++ elojs/exercise12.js
@@ -17,3 +17,18 @@ do(define(sum, fun(array,
    print(sum(array(1, 2, 3))))
 `);
 // → 6
+
+
+//12.3
+function skipSpace(string) {
+  let skip = string.match(/^(\s|\#.*)*/);
+  return string.slice(skip[0].length);
+}
+
+console.log(parse("# hello\nx"));
+// → {type: "word", name: "x"}
+
+console.log(parse("a # one\n   # two\n()"));
+// → {type: "apply",
+//    operator: {type: "word", name: "a"},
+//    args: []}
