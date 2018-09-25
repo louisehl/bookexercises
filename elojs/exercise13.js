b6f95a632bd0b42d4d821c981dc8385070ff3a2e
Louise <Louiselastein@hotmail.com>
2018-09-25 21:07:49 +0200

    13.3

--- elojs/exercise13.js
+++ elojs/exercise13.js
@@ -80,3 +80,32 @@
   console.log(byTagName(para, "span").length);
   // → 2
 </script>
+
+//13.3
+
+<!doctype html>
+
+<style>body { min-height: 200px }</style>
+<img src="img/cat.png" id="cat" style="position: absolute">
+<img src="img/hat.png" id="hat" style="position: absolute">
+
+<script>
+  let cat = document.querySelector("#cat");
+  let hat = document.querySelector("#hat");
+
+  let angle = 0;
+  let lastTime = null;
+  function animate(time) {
+    if (lastTime != null) angle += (time - lastTime) * 0.001;
+    lastTime = time;
+    cat.style.top = (Math.sin(angle) * 40 + 40) + 100 + "px";
+    cat.style.left = (Math.cos(angle) * 200 + 230) + 100 + "px";
+
+    hat.style.top = ((Math.sin(angle) * 40 + 40) - Math.sin(angle) * 80) + 100 + "px";
+    hat.style.left = ((Math.cos(angle) * 200 + 230) - Math.cos(angle) * 80) + 100 + "px";
+
+
+    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
+</script>
