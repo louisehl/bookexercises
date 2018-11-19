e04fda27575555b5de04ccb33b33a43f3c598d07
Louise Hessellund Lastein <louise.lastein@lego.com>
2018-11-19 20:29:40 +0100

    SPA part 1

--- coursera/angular/conFusion/src/app/app.module.ts
+++ coursera/angular/conFusion/src/app/app.module.ts
@@ -11,12 +11,14 @@ import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import 'hammerjs';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
@@ -46,7 +48,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
