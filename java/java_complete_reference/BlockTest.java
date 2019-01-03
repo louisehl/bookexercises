d2b7c7aa6860c9bca51d67e5a2ac99260d73213a
Louise Hessellund Lastein <louise.lastein@lego.com>
2019-01-03 21:12:28 +0100

    json server and beginning java

---
+++ java/java_complete_reference/BlockTest.java
@@ -0,0 +1,17 @@
+/*
+ * Demonstrate a block of code
+ */
+
+class BlockTest {
+    public static void main(String args[]) {
+        int x, y;
+
+        y = 20;
+
+        for (x = 0; x < 10; x++) {
+            System.out.println("This is x: " + x);
+            System.out.println("This is y: " + y);
+            y = y - 2;
+        }
+    }
+}
---
+++ java/java_complete_reference/Example.java
@@ -0,0 +1,9 @@
+/*
+ * Example.java
+ */
+
+class Example {
+    public static void main(String args[]) {
+        System.out.println("This is a simple Java Program.");
+    }
+}
