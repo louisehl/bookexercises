84d7d4ab96fdad134bde7ae0c78edef9ff338643
Louise Hessellund Lastein <louise.lastein@lego.com>
2019-01-08 21:13:07 +0100

    Constructor

--- java/java_complete_reference/chapter6/BoxDemo.java
+++ java/java_complete_reference/chapter6/BoxDemo.java
@@ -3,6 +3,12 @@ class Box {
     double height;
     double depth;
 
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
     double volume() {
         return width * height * depth;
     }
@@ -10,12 +16,9 @@ class Box {
 
 class BoxDemo {
     public static void main(String args[]) {
-        Box mybox = new Box();
+        Box mybox = new Box(10, 15, 20);
         double vol;
 
-        mybox.width = 10;
-        mybox.height = 20;
-        mybox.depth = 15;
 
         //display volume
         System.out.println("Volume is: " + mybox.volume());
